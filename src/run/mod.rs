de8a1ce10bc497d667477e960a732b2d3eb6f805
Shane Osbourne <shane.osbourne8@gmail.com>
2018-05-28 21:07:40 +0100

    fix file path

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::io::Error;
 
 pub fn run(run_context: &RunContext) -> Result<IncomingCommand, Error> {
-    let docker_compose_text = include_str!("templates/docker-compose.yml");
+    let docker_compose_text = include_str!("../templates/docker-compose.yml");
 
     let docker_compose_build_args = vec!["-f", "-", "up", "-d"]
         .iter()
