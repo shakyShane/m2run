cc79d97a2345a52bbef1a89c207ab2d3529fd0d5
Shane Osbourne <shane.osbourne8@gmail.com>
2018-06-01 12:31:29 +0200

    general fn refactorings

--- src/context.rs
+++ src/context.rs
@@ -10,6 +10,7 @@ use std::process::Command;
 use std::process::ExitStatus;
 use std::process::Stdio;
 use std::collections::HashMap;
+use std::env::Args;
 
 #[derive(Debug)]
 pub struct RunContext {
@@ -29,6 +30,57 @@ pub enum RunMode {
     Execute,
 }
 
+pub const M2RUN_CONTEXT_NAME: &'static str = "M2RUN_CONTEXT_NAME";
+
+pub fn get_run_context() -> Result<RunContext, String> {
+    has_docker()
+        .and_then(|_x| get_options())
+        .and_then(|options: Options| {
+            is_valid_dir(&options.cwd)
+                .and_then(verify_files)
+                .and_then(set_working_dir)
+                .and_then(|_| create_run_context(options, env::args()))
+        })
+}
+
+pub fn create_run_context(options: options::Options, mut args: Args) -> Result<RunContext, String> {
+
+    let ctx_name = get_context_name(&options);
+    let cmd  = select_cmd(args.nth(1));
+    let mode = select_mode(options.flags.get("run_mode"));
+    let user = select_user(options.flags.get("user"));
+    let default_env = get_default_env(&ctx_name);
+
+    Ok(RunContext {
+        cwd: options.cwd.to_path_buf(),
+        name: ctx_name.to_string(),
+        command: cmd,
+        cwd_file_name: ctx_name.to_string(),
+        options,
+        mode,
+        env: default_env,
+        user: user.to_string()
+    })
+}
+
+fn get_context_name(options: &options::Options) -> String {
+    let cwd_as_buf = options.cwd.to_path_buf();
+    let context_name = cwd_as_buf.file_name().unwrap();
+    let context_name_as_string = context_name.to_string_lossy().to_string();
+    context_name_as_string
+}
+
+fn get_default_env(name: &String) -> HashMap<String, String> {
+    let mut env: HashMap<String, String> = HashMap::new();
+
+    env.insert(
+        M2RUN_CONTEXT_NAME.to_string(),
+        name.to_string(),
+    );
+
+    env
+}
+
 fn select_mode(set_mode: Option<&String>) -> RunMode {
     match set_mode {
         Some(mode) => match mode.as_str() {
@@ -40,59 +92,18 @@ fn select_mode(set_mode: Option<&String>) -> RunMode {
     }
 }
 
-fn select_cmd(maybe_command: Option<String>) -> String {
-    maybe_command.or(Some("contrib".to_string())).unwrap()
-}
-
-pub fn create_run_context(options: options::Options) -> Result<RunContext, String> {
-    let cwd_as_buf = options.cwd.to_path_buf();
-    let context_name = cwd_as_buf.file_name().unwrap();
-    let context_name_as_string = context_name.to_string_lossy();
-
-    let cmd = select_cmd(env::args().nth(1));
-    let mode = select_mode(options.flags.get("run_mode"));
-
-    let user = match options.flags.get("user") {
+fn select_user<'a>(set_user: Option<&String>) -> &'a str {
+    match set_user {
         Some(user) => match user.as_str() {
             "root" | "r" => "root",
             _ => "www-data"
         },
         None => "www-data"
-    };
-
-    let mut env: HashMap<String, String> = HashMap::new();
-
-    env.insert(
-        "M2RUN_CONTEXT_NAME".to_string(),
-        context_name_as_string.to_string(),
-    );
-
-    Ok(RunContext {
-        cwd: options.cwd.to_path_buf(),
-        name: context_name_as_string.to_string(),
-        command: cmd,
-        cwd_file_name: options
-            .cwd
-            .file_name()
-            .unwrap()
-            .to_string_lossy()
-            .to_string(),
-        options: options,
-        mode,
-        env,
-        user: user.to_string()
-    })
+    }
 }
 
-pub fn get_run_context() -> Result<RunContext, String> {
-    has_docker()
-        .and_then(|_x| get_options())
-        .and_then(|options: Options| {
-            is_valid_dir(&options.cwd)
-                .and_then(verify_files)
-                .and_then(set_working_dir)
-                .and_then(|_| create_run_context(options))
-        })
+fn select_cmd(maybe_command: Option<String>) -> String {
+    maybe_command.or(Some("contrib".to_string())).unwrap()
 }
 
 fn set_working_dir(path_buf: &PathBuf) -> Result<(), String> {
